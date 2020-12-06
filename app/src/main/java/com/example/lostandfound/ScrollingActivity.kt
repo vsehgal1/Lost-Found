e36a69bca1524e4a5acd117e1e799361e94d3500
srivats <sncars33@gmail.com>
2020-12-06 15:57:01 -0500

    fixing pictures filepath

--- app/src/main/java/com/example/lostandfound/ScrollingActivity.kt
+++ app/src/main/java/com/example/lostandfound/ScrollingActivity.kt
@@ -62,7 +62,7 @@ class ScrollingActivity : AppCompatActivity() {
         //new changes here
         var arr1 = ArrayList<String>()
         arr1.add("https://media-cdn.yoogiscloset.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/3/1/315012_01_1.jpg")
-        
+
         //testing firebase
 //        fbref.newSubmission(
 //            UID,
@@ -91,16 +91,10 @@ class ScrollingActivity : AppCompatActivity() {
         listView.adapter = itemAdapter
 
         addBut.setOnClickListener {
-            dataArray.add(
-                LostItem("asd","fdgd",
-                    "https://cdn.shopify.com/s/files/1/0345/1441/products/Spare-Keys_1024x1024.png?v=1421177358",
-                    "Keys",
-                    "Tawes Hall",
-                    "Keys of type Keys",
-                    "11/10/2020",
-                    "12/04/2020"
-                )
-            )
+            val intent = Intent(this@ScrollingActivity, EnterLostItemActivity::class.java)
+            intent.putExtra("UID", UID)
+            startActivity(intent)
+
             itemAdapter.flag = true
             itemAdapter.notifyDataSetChanged()
         }
@@ -180,22 +174,22 @@ class ScrollingActivity : AppCompatActivity() {
         }
 
         // create search view object
-            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                override fun onQueryTextSubmit(query: String?): Boolean {
-                    return false
-                }
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
 
-                override fun onQueryTextChange(newText: String?): Boolean {
-                    Log.i("Click", "Reached searchView listener")
-                    if (TextUtils.isEmpty(newText)) {
-                        itemAdapter.filter("")
-                        listView.clearTextFilter()
-                    } else newText?.let { itemAdapter.filter(it) }
-                    return false
-                }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                Log.i("Click", "Reached searchView listener")
+                if (TextUtils.isEmpty(newText)) {
+                    itemAdapter.filter("")
+                    listView.clearTextFilter()
+                } else newText?.let { itemAdapter.filter(it) }
+                return false
+            }
 
-            })
-        }
+        })
+    }
 
 //    override fun onStart() {
 //        super.onStart()
@@ -204,74 +198,74 @@ class ScrollingActivity : AppCompatActivity() {
 //
 //    }
 
-        override fun onCreateOptionsMenu(menu: Menu): Boolean {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            menuInflater.inflate(R.menu.menu_scrolling, menu)
-            return true
-        }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_scrolling, menu)
+        return true
+    }
 
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
 
-            return when (item.itemId) {
-                R.id.action_settings -> true
-                else -> super.onOptionsItemSelected(item)
-            }
-        }
-
-        fun displayArray(arrList : ArrayList<LostItem>): String {
-            for (i in arrList){
-                Log.i("Click", i.name + i.id)
-            }
-            return "done"
-        }
-        fun dispGlobalArray(arrList : ArrayList<LostItemSubmission>): String {
-            Log.i("Click", "Global List:")
-            for (i in arrList){
-                Log.i("Click", i.name +" " +i.id+ " "+ i.pictureURLs[0])
-            }
-            return "done"
-        }
-
-        fun updateLocalList(){
-//            Log.i("Click", "come to updateLocalList")
-//            Log.i("Click", dispGlobalArray(fbref.lostItemsList))
-            var listener = object: OnGetDataListener {
-                override fun onSuccess(snapshot: Object) {
-                    var list = snapshot as ArrayList<LostItemSubmission>
-                    fbref.lostItemsList = list
-                    Log.i("Click", "Getting subs")
-                    Log.i("Click", list.toString())
-                    Log.i("Click", "Current length: "+ list.size)
-
-                }
-
-                override fun onStart() {
-                }
-
-                override fun onFailure(error: Object) {
-                    var err = error as DatabaseError
-                    Log.i(TAG, err.message)
-                    Toast.makeText(applicationContext,
-                        "NETWORK ERROR - Please check your network connection",
-                        Toast.LENGTH_SHORT).show()
-                }
-
-            }
-            fbref.fetchSubmissionsList(listener)
-            dataArray.clear()
-            for(i:LostItemSubmission in fbref.lostItemsList){
-                dataArray.add(LostItem(i.userid,i.id,i.pictureURLs[0],i.name, i.location, i.description, i.dateFound, i.dateSubmitted))
-            }
-//            Log.i("Click", "Display local list")
-//            Log.i("Click", dataArray.toString())
-        }
-
-        companion object {
-            fun create(): FirebaseRef = FirebaseRef();
-            const val TAG = "Lost&Found";
-            const val IMAGE_PATH = "images/";
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
         }
     }
+
+    fun displayArray(arrList : ArrayList<LostItem>): String {
+        for (i in arrList){
+            Log.i("Click", i.name + i.id)
+        }
+        return "done"
+    }
+    fun dispGlobalArray(arrList : ArrayList<LostItemSubmission>): String {
+        Log.i("Click", "Global List:")
+        for (i in arrList){
+            Log.i("Click", i.name +" " +i.id+ " "+ i.pictureURLs[0])
+        }
+        return "done"
+    }
+
+    fun updateLocalList(){
+//            Log.i("Click", "come to updateLocalList")
+//            Log.i("Click", dispGlobalArray(fbref.lostItemsList))
+        var listener = object: OnGetDataListener {
+            override fun onSuccess(snapshot: Object) {
+                var list = snapshot as ArrayList<LostItemSubmission>
+                fbref.lostItemsList = list
+                Log.i("Click", "Getting subs")
+                Log.i("Click", list.toString())
+                Log.i("Click", "Current length: "+ list.size)
+
+            }
+
+            override fun onStart() {
+            }
+
+            override fun onFailure(error: Object) {
+                var err = error as DatabaseError
+                Log.i(TAG, err.message)
+                Toast.makeText(applicationContext,
+                    "NETWORK ERROR - Please check your network connection",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        fbref.fetchSubmissionsList(listener)
+        dataArray.clear()
+        for(i:LostItemSubmission in fbref.lostItemsList){
+            dataArray.add(LostItem(i.userid,i.id,i.pictureURLs[0],i.name, i.location, i.description, i.dateFound, i.dateSubmitted))
+        }
+//            Log.i("Click", "Display local list")
+//            Log.i("Click", dataArray.toString())
+    }
+
+    companion object {
+        fun create(): FirebaseRef = FirebaseRef();
+        const val TAG = "Lost&Found";
+        const val IMAGE_PATH = "images/";
+    }
+}
